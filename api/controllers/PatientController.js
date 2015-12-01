99c3ba67aea1bc9f0a5d592f985dbf32494bc096
hasib hasan tarafder <ninjarnab@gmail.com>
2015-12-01 18:44:23 +0600

    add findPatientByNID method to the patient controller

--- api/controllers/PatientController.js
+++ api/controllers/PatientController.js
@@ -6,6 +6,25 @@
  */
 
 module.exports = {
-	
-};
+    findPatientByNID: function(req, res) {
+        var id = req.param(id);
+        Patient.findOne({
+            NID: id
+        }).exec(function(err, patient) {
+            if (err) {
+                return res.json({
+                    error: err
+                });
+            }
 
+            if (patient === undefined) {
+                return res.notFound();
+            } else {
+                return res.json({
+                    notFound: false,
+                    patientData: patient
+                });
+            }
+        })
+    }
+};
